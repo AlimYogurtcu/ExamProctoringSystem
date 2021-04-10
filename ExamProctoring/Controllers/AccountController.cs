92212330a5e4f80dad70f0ddcb14d95940819850
Alim Yoğurtçu <alimyogurtcu@outlook.com>
2021-04-10 13:50:50 +0300

    Dizayn tamamlandı. Giriş sayfası database bağlandı.

--- ExamProctoring/Controllers/AccountController.cs
+++ ExamProctoring/Controllers/AccountController.cs
@@ -1,18 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using ExamProctoring.Models;
-using MySqlConnector;
 
 namespace ExamProctoring.Controllers
 {
     public class AccountController : Controller
     {
         // GET: Account
-        string connectionString = "SERVER=localhost;DATABASE=schooldb; UID=root; Password=mysql123;"; // connection string for my sql
-        MySqlConnection cnnct;
+        string connectionString = "Data Source=tcp:examproctoringdatabase.database.windows.net,1433;Initial Catalog=ExamProctoring_db;User Id=examproctoring@examproctoringdatabase;Password=Exam1907";
+        
+        SqlConnection cnnct;
         [HttpGet]
         public ActionResult Login()
         {
@@ -23,27 +24,26 @@ namespace ExamProctoring.Controllers
         public ActionResult Verify(Account acc)
         {
             bool isProctor = false;
-            cnnct = new MySqlConnection(connectionString); 
-            string query = "SELECT * FROM account_info WHERE account_username = '"+acc.accountUsername+"' and account_password = '"+acc.accountPassword+"';";
+            cnnct = new SqlConnection(connectionString); 
+            string query = "SELECT * FROM account_information WHERE account_username = '"+acc.accountUsername+"' and account_password = '"+acc.accountPassword+"';";
             /*string queryTwo = "INSERT INTO `account_info` (`id`, `account_username`, `account_password`, `account_role`) VALUES (NULL, '21704737', 'pass123', 'Student')";
 
             MySqlCommand cmmndTwo = new MySqlCommand(queryTwo, cnnct);*/
-            MySqlCommand cmmnd = new MySqlCommand(query,cnnct);
-            MySqlDataReader reader;
+            SqlCommand cmmnd = new SqlCommand(query,cnnct);
+            SqlDataReader reader;
             
             cnnct.Open();
 
             /*cmmndTwo.ExecuteNonQuery();*/
 
             reader = cmmnd.ExecuteReader();
-
             if (reader.Read())
             {
-                if(reader.GetString("account_role") == "Student")
+                if (reader.GetString(3) == "Student")
                 {
                     isProctor = false;
                 }
-                else if(reader.GetString("account_role") == "Teacher")
+                else if(reader.GetString(3) == "Teacher")
                 {
                     isProctor = true;
                 }
